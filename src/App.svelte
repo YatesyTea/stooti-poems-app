f6c675e0d71397eec995e157cb9bfacc4a74914d
YatesyTea <39750168+YatesyTea@users.noreply.github.com>
2023-10-20 14:36:41 +0100

    update picture

--- src/App.svelte
+++ src/App.svelte
@@ -9,10 +9,10 @@
 <main>
   <img
     class="rounded-pic"
-    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Noi4l9ejSc3y5vqicwihngHaJ4%26pid%3DApi&f=1&ipt=37f1410e6a90859d1d267f031e8b2216cb92a6e49fec81de147ec520c0f8e6cd&ipo=images"
+    src="src/assets/spachisia.jpg"
     alt='stooti'
   />
-  <h1>Stuti's Random Poem of the Day</h1>
+  <h1>Stooti's Random Poem of the Day</h1>
   <p>Today's Date is {day}-{month}-{year}</p>
   <div>
     <Card />
